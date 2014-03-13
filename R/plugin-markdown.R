aa297f13a3e34ae32e3e0da387ebcf5c58fcbf4e
hadley <h.wickham@gmail.com>
2014-03-13 08:59:27 -0500

    Add markdown plugin

---
+++ R/plugin-markdown.R
@@ -0,0 +1,30 @@
+#' Convert markdown to html
+#'
+#' Use the \pkg{markdown} package. Markdown inputs are converted to html
+#' and their extension changed to \code{.html}.
+#'
+#' @param pattern Regular expression used to identify markdown files.
+#' @export
+#' @examples
+#' static_site <- rsmith_demo("static-site")
+#' static_site %>% preview()
+#' static_site %>% add_plugin(use_markdown()) %>% preview()
+use_markdown <- function(pattern = "\\.md$") {
+  if (!is_installed("markdown")) {
+    stop("Please install the markdown package", call. = FALSE)
+  }
+
+  plugin("markdown", function(file) {
+    if (!grepl(pattern, path(file))) return(file)
+
+    if (nzchar(file$contents)) {
+      html <- markdown::markdownToHTML(text = file$contents, fragment.only = TRUE)
+      file$contents <- html
+    }
+
+    path <- tools::file_path_sans_ext(file$metadata$.path)
+    file$metadata$.path <- paste0(path, ".html")
+
+    file
+  })
+}
