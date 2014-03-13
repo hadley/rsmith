239f1713772ece27ab9709b5d6da4b7311dfde30
hadley <h.wickham@gmail.com>
2014-03-12 20:34:29 -0500

    Update remove drafts plugin to new style

--- R/plugins-draft.R
+++ R/plugins-draft.R
@@ -1,8 +1,19 @@
+#' Remove draft file.
+#'
+#' Any file with \code{draft: true} in the metadata will be removed.
+#'
+#' @param rsmith An  \code{\link{rsmith}} object.
+#' @return An modified \code{\link{rsmith}} object.
+#' @export
+#' @family plugins
+#' @examples
+#' static_site <- rsmith_demo("static-site")
+#' static_site$files[[1]]$metadata$draft <- TRUE
+#' remove_drafts(static_site)
+remove_drafts <- function(rsmith) {
+  is_draft <- vapply(rsmith$files, function(x) isTRUE(x$metadata$draft),
+    logical(1))
+  rsmith$files <- rsmith$files[!is_draft]
 
-use_draft <- function(rsmith) transform_each(draft)
-
-draft <- function(file, rsmith) {
-  if (isTRUE(file$metadata$draft)) return(NULL)
-  file
+  rsmith
 }
-
