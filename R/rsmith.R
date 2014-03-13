0fcb3b2a591f9e857fd2ceec6e8d4a3fcbaeea36
hadley <h.wickham@gmail.com>
2014-03-13 05:57:59 -0500

    Make rsmith return reactive objects

--- R/rsmith.R
+++ R/rsmith.R
@@ -1,14 +1,15 @@
+#' @examples
+#' rsmith_demo("static-site")
 rsmith <- function(src = "src", dest = "build") {
   src <- normalizePath(src, mustWork = TRUE)
   dest <- normalizePath(dest, mustWork = FALSE)
-  metadata <- list(.src = src, .dest = dest)
+  metadata <- reactiveValues(.src = src, .dest = dest)
 
-  old <- setwd(src)
-  on.exit(setwd(old))
-
-  paths <- dir(recursive = TRUE, full.names = TRUE)
+  paths <- dir(src, recursive = TRUE, full.names = TRUE)
   paths <- sub("^\\./", "", paths)
-  files <- lapply(paths, read_file_with_metadata)
+  files <- lapply(paths, function(x) {
+    reactiveFileReader(250, NULL, x, read_file_with_metadata)
+  })
 
   rsmith_obj(metadata, files)
 }
@@ -22,16 +23,17 @@ rsmith_obj <- function(metadata, files) {
 
 #' @export
 print.rsmith <- function(x, ...) {
-  cat("<rsmith> ", x$metadata$.src , " -> ", x$metadata$.dest, "\n", sep = "")
-  print_metadata(x$metadata)
+  isolate(cat("<rsmith> ", x$metadata$.src , " -> ", x$metadata$.dest, "\n",
+    sep = ""))
+  # print_metadata(metadata)
 
   cat("Files:\n")
-  files <- paste(vapply(x$files, path, character(1)), collapse = ", ")
-  cat(strwrap(files, indent = 2, exdent = 2), sep = "\n")
+  files <- isolate(lapply(x$files, function(f) f()))
+  paths <- paste(vapply(files, path, character(1)), collapse = ", ")
+  cat(strwrap(paths, indent = 2, exdent = 2), sep = "\n")
 }
 
 is.rsmith <- function(x) inherits(x, "rsmith")
-
 
 rsmith_demo <- function(name, ...) {
   path <- system.file("demo", name, package = "rsmith")
